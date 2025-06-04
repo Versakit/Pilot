354eebbf551ca4c423627d2faabfc873260d1ecb
Jannik <1337741710@qq.com>
2025-06-04 16:44:52 +0800

    chore: 更新主题组件，移除自定义布局并更改指令库导入路径

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,16 +1,14 @@
 import Theme from 'vitepress/theme'
-import MyLayout from './Layout.vue'
 
 // 引入样式
 import './index.scss'
 import 'virtual:group-icons.css'
 
 // 引入组件库
-import { VersakitDirectives } from '@versakit/directives'
+import { VersakitDirectives } from 'pilot'
 
 export default {
   ...Theme,
-  Layout: MyLayout,
   enhanceApp({ app }) {
     app.use(VersakitDirectives)
   },
